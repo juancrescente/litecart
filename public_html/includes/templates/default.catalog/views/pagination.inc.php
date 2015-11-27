4f2cd60745808a5452e416a1d93620b1dd332ad4
timint <info@tim-international.net>
2015-11-27 06:26:41 +0100

    * Bootstrap pagination

--- public_html/includes/templates/default.catalog/views/pagination.inc.php
+++ public_html/includes/templates/default.catalog/views/pagination.inc.php
@@ -2,10 +2,10 @@
   <ul class="pagination">
   <?php foreach($items as $item) { ?>
     <?php if ($item['disabled']) ?>
-    <li class="disabled<?php echo ($item['active']) ? 'active' : ''; ?>"><span><?php echo $item['title']; ?></span></li>
+    <li class="disabled<?php if ($item['active']) echo ' active'; ?>"><span><?php echo $item['title']; ?></span></li>
     <?php } else { ?>
-    <li class="<?php echo ($item['active']) ? 'active' : ''; ?>"><a href="<?php echo htmlspecialchars($item['link']); ?>"><?php echo $item['title']; ?></a></li>
-    <?php }
+    <li class="<?php if ($item['active']) echo ' active'; ?>"><a href="<?php echo htmlspecialchars($item['link']); ?>"><?php echo $item['title']; ?></a></li>
+    <?php } ?>
   <?php } ?>
   </ul>
 </nav>
