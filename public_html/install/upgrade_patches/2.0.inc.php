70f0d33f7620fc68dc12fe9608c4559bfa70b78b
timint <info@tim-international.net>
2015-12-13 19:12:45 +0100

    + Upgrade patch 2.0

---
+++ public_html/install/upgrade_patches/2.0.inc.php
@@ -0,0 +1,22 @@
+<?php
+  
+  $deleted_files = array(
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'ext/responsiveslider/',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'fancybox/',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'admin.catalog/images/fancybox/',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'admin.catalog/styles/fancybox.css',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.catalog/images/fancybox/',
+    FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATES .'default.catalog/styles/fancybox.css',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery/jquery-1.11.3.min.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery/jquery-1.11.3.min.map',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery/jquery-migrate-1.2.1.min.js',
+    FS_DIR_HTTP_ROOT . WS_DIR_EXT .'jquery/jquery/jquery.tabs.js',
+  );
+  
+  foreach ($deleted_files as $pattern) {
+    if (!file_delete($pattern)) {
+      die('<span class="error">[Error]</span></p>');
+    }
+  }
+
+?>
