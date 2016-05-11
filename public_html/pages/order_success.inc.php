9a794b757ca28de7f7cd68b2b1385c1118f1ec85
timint <info@tim-international.net>
2016-05-11 20:18:55 +0200

    + Order module - Order action - Order success

--- public_html/pages/order_success.inc.php
+++ public_html/pages/order_success.inc.php
@@ -5,26 +5,26 @@
 
   breadcrumbs::add(language::translate('title_checkout', 'Checkout'), document::ilink('checkout'));
   breadcrumbs::add(language::translate('title_order_success', 'Order Success'));
-  
+
   $order = new ctrl_order('resume');
-  
+
   $payment = new mod_payment();
-  
-  $order_success = new mod_order_success();
-  
+
+  $order_module = new mod_order();
+
   if (empty($order->data['id'])) die('Error: Missing session order object');
-  
+
   cart::reset();
-  
+
   $modal_id = functions::draw_modal();
 
   $_page = new view();
-  
+
   $_page->snippets = array(
     'printable_link' => document::ilink('printable_order_copy', array('order_id' => $order->data['id'], 'checksum' => functions::general_order_public_checksum($order->data['id']), 'media' => 'print')),
     'payment_receipt' => $payment->receipt($order),
-    'order_success_modules_output' => $order_success->process($order),
+    'order_success_modules_output' => $order_module->success($order),
   );
-  
+
   echo $_page->stitch('pages/order_success');
 ?>
