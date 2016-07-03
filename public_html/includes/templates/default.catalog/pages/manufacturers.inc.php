0215b314361ba610cbb19e6c7be80f1c4c01073d
timint <info@tim-international.net>
2016-07-03 14:42:02 +0200

    * Box manufacturers

--- public_html/includes/templates/default.catalog/pages/manufacturers.inc.php
+++ public_html/includes/templates/default.catalog/pages/manufacturers.inc.php
@@ -1,3 +1,19 @@
 <!--snippet:notices-->
 
-{snippet:box_manufacturers}
+<div id="manufacturers">
+  <h1 class="title"><?php echo language::translate('title_manufacturers', 'Manufacturers'); ?></h1>
+  <div class="manufacturers row half-gutter">
+      <?php foreach($manufacturers as $manufacturer) { ?>
+      <div class="col-md-3">
+        <div class="thumbnail">
+          <a class="link" href="<?php echo htmlspecialchars($manufacturer['link']); ?>" title="<?php echo htmlspecialchars($manufacturer['name']); ?>">
+            <div class="image-wrapper">
+              <img class="img-responsive" src="<?php echo htmlspecialchars($manufacturer['image']['thumbnail']); ?>" srcset="<?php echo htmlspecialchars($manufacturer['image']['thumbnail']); ?> 1x, <?php echo htmlspecialchars($manufacturer['image']['thumbnail_2x']); ?> 2x" alt="<?php echo htmlspecialchars($manufacturer['name']); ?>" />
+            </div>
+            <div class="caption text-center"><?php echo $manufacturer['name']; ?></div>
+          </a>
+        </div>
+      </div>
+      <?php } ?>
+  </div>
+</div>
